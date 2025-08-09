88da146c2ee08506de9c2befcc621019f2c30132
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-08-09 12:07:32 +0530

    Add files via upload

---
+++ INDEX_OF_THE_FIRST_OCCURRENCE_OF_THE_STRING.py
@@ -0,0 +1,9 @@
+#Leet code Questions Solutions. 
+
+#Date : 09/08/2025
+#Problem 27 : Remove Element
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        result = haystack.find(needle)
+        return result
