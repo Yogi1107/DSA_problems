ef0c3268bc3b24a89013db582c3fe2ca583312bf
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-08-15 14:35:44 +0530

    Add files via upload

---
+++ perfect_number.py
@@ -0,0 +1,20 @@
+#lettcode Problem
+
+#date : 15/08/2025
+#Problem : 507 : Perfect number
+
+
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+        total = 1  
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                total += i
+                if i != num // i:
+                    total += num // i
+            i += 1
+        
+        return total == num
