49656ed2615c11ac06d05fd25029b6bae7ceb0a1
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-11-19 18:27:29 +0530

    Create keep_multiplying_find_values_by_2.py

---
+++ keep_multiplying_find_values_by_2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        for i in nums:
+            if original in nums:
+                original = original * 2
+        
+        return original
+        
+# class Solution:
+#     def findFinalValue(self, nums: List[int], original: int) -> int:
+
+        
+#         flag = True
+
+#         while flag:
+#             if original in nums:
+#                 original = original * 2
+#             else:
+#                 return original
+        
