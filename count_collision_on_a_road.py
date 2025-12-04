e19b4f34839b666c94d5bcdb555aa0be68357342
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-12-04 19:56:20 +0530

    Update count_collision_on_a_road.py

--- count_collision_on_a_road.py
+++ count_collision_on_a_road.py
@@ -7,3 +7,29 @@ class Solution:
         # Every 'L' or 'R' contributes 1 collision
         # 'S' contributes 0
         return len(s) - s.count('S')
+
+
+
+# class Solution {
+#     public int countCollisions(String directions) {
+#         int left = 0;
+#         while (left < directions.length() && directions.charAt(left) == 'L') {
+#             left++;
+#         }
+
+#         int right = directions.length() - 1;
+#         while (right >= 0 && directions.charAt(right) == 'R') {
+#             right--;
+#         }
+
+#         int collisions = 0;
+#         for (int i = left; i <= right; i++) {
+#             if (directions.charAt(i) != 'S') {
+#                 collisions++;
+#             }
+#         }
+
+#         return collisions;
+#     }
+# }
+
