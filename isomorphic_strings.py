bff08dbdd4f33dbd9d28b037a64aadc4dce5e414
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-12-15 23:29:21 +0530

    Refactor isIsomorphic method for clarity and efficiency

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,18 +1,41 @@
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+
+#         mapST = {}
+#         mapTS = {}
+
+#         for c1, c2 in zip(s, t):
+#             if c1 in mapST and mapST[c1] != c2:
+#                 return False
+#             if c2 in mapTS and mapTS[c2] != c1:
+#                 return False
+
+#             mapST[c1] = c2
+#             mapTS[c2] = c1
+
+#         return True
+
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        map_s = {}
+        t_used = set()
 
-        mapST = {}
-        mapTS = {}
+        for ch_s, ch_t in zip(s,t):
 
-        for c1, c2 in zip(s, t):
-            if c1 in mapST and mapST[c1] != c2:
-                return False
-            if c2 in mapTS and mapTS[c2] != c1:
-                return False
+            if ch_s not in map_s:
 
-            mapST[c1] = c2
-            mapTS[c2] = c1
+                if ch_t in t_used:
+                    return False
+                map_s[ch_s] = ch_t
+                t_used.add(ch_t)
 
+            else:
+                if map_s[ch_s] != ch_t:
+                    return False
+
+        
         return True
+            
