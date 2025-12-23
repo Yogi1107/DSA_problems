f792489fc5c05727a8f490e6f3e8bdb33ff48060
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-12-23 19:15:58 +0530

    Create find_the_duplicate_number.py

---
+++ find_the_duplicate_number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[0]
+
+        # Phase 1: Detect cycle
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        # Phase 2: Find cycle entrance
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
