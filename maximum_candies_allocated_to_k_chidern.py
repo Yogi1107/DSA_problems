42b7b7478851fe3392e2791db89264b7f29dd559
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-12-25 15:57:49 +0530

    Create maximum_candies_allocated_to_k_chidern.py

---
+++ maximum_candies_allocated_to_k_chidern.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        if sum(candies) < k:
+            return 0
+        
+        left, right = 1, max(candies)
+        answer = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            count = 0
+            for c in candies:
+                count += c // mid
+            
+            if count >= k:
+                answer = mid      
+                left = mid + 1    
+            else:
+                right = mid - 1   
+        
+        return answer
