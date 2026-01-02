3e7ba2bc82db570efedbee0096d73a1f61cde3a7
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2026-01-02 11:24:16 +0530

    Update min_steps_to_make_two_strings_anagram.py

--- min_steps_to_make_two_strings_anagram.py
+++ min_steps_to_make_two_strings_anagram.py
@@ -1,13 +1,19 @@
+# from collections import Counter
+
+# class Solution:
+#     def minSteps(self, s: str, t: str) -> int:
+#         count_s = Counter(s)
+#         count_t = Counter(t)
+        
+#         steps = 0
+#         for ch in count_t:
+#             if count_t[ch] > count_s[ch]:
+#                 steps += count_t[ch] - count_s[ch]
+        
+#         return steps
+
 from collections import Counter
 
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        count_s = Counter(s)
-        count_t = Counter(t)
-        
-        steps = 0
-        for ch in count_t:
-            if count_t[ch] > count_s[ch]:
-                steps += count_t[ch] - count_s[ch]
-        
-        return steps
+        return sum((Counter(t) - Counter(s)).values())
