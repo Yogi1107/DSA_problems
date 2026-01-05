b4eae3a11fd128de140c1f50804dfdb80b7189ec
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2026-01-05 19:21:33 +0530

    Create longest_consecutive_subsequence.py

---
+++ longest_consecutive_subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for num in numSet:
+            # check if num is the start of a sequence
+            if num - 1 not in numSet:
+                current = num
+                length = 1
+
+                while current + 1 in numSet:
+                    current += 1
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
