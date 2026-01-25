79ba140860f657a7c6397e2ba7e9b5ac51f0a4d2
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2026-01-25 15:08:34 +0530

    Implement base 7 conversion in base_7.py

---
+++ base_7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        sign = "-" if num < 0 else ""
+        num = abs(num)
+
+        result = []
+
+        while num != 0:
+            result.append(str(num % 7))
+            num //= 7
+
+        return sign + ''.join(reversed(result))
