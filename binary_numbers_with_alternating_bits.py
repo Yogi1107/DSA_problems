65d572fd14b13cbc65ed77cea66728b996f330e1
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2026-02-18 20:33:48 +0530

    Update hasAlternatingBits to check binary format
    Refactor hasAlternatingBits method to use binary representation for checking alternating bits.

--- binary_numbers_with_alternating_bits.py
+++ binary_numbers_with_alternating_bits.py
@@ -1,4 +1,14 @@
+# class Solution:
+#     def hasAlternatingBits(self, n: int) -> bool:
+#         x = n ^ (n >> 1)
+#         return (x & (x + 1)) == 0
+
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        x = n ^ (n >> 1)
-        return (x & (x + 1)) == 0
+        binary = bin(n)[2:]   # remove '0b'
+
+        for i in range(1, len(binary)):
+            if binary[i] == binary[i - 1]:
+                return False
+
+        return True
